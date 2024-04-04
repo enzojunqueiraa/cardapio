99fb3676881fc128f925443180e44da76ed2134f
enzobjunqueiraa <enzobjunqueira@gmail.com>
2024-04-04 14:08:29 -0300

    commit de hj

--- src/screens/CadastroProduto.tsx
+++ src/screens/CadastroProduto.tsx
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
-import { Image, StatusBar, StyleSheet, Text,TextInput, TouchableOpacity, View } from "react-native";
-import { launchCamera } from "react-native-image-picker";
-
+import { Image, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
+import axios from 'axios';
 
 const CadastroProduto: React.FC = () => {
 
     const [produtos, setProdutos] = useState<Produto[]>([]);
-    const [nome, setNome] = useState<string>("");
-    const [preco, setPreco] = useState<string>("");
-    const [ingredientes, setIngredientes] = useState<string>("");
-    const [imagem, setImagem] = useState<any>("");
+    const [nome, setNome] = useState<string>('');
+    const [preco, setPreco] = useState<string>('');
+    const [ingredientes, setIngredientes] = useState<string>('');
+    const [imagem, setImagem] = useState<any>('');
 
-    const CadastroProduto = async () =>{
+    const cadastrarProduto = async() => {
+        try {
+            const formData = new FormData();
+            formData.append('nome', nome);
+            formData.append('preco', preco);
+            formData.append('ingredientes', ingredientes);
+            formData.append('imagem', {
+                uri: imagem,
+                type: 'image/jpeg',
+                name:new Date()+ '.jpg'
+            });
 
+            const response = await axios.post('http://10.137.11.226:8000/api/produtos', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        }catch(error) {
+            console.log(error);
+        }
     }
+
+
+
 
     const abrirCamera = () => {
         const options = {
@@ -35,6 +56,30 @@ const CadastroProduto: React.FC = () => {
             }
         });
     }
+    
+    const selecionarImagem = () => {
+        const options = {
+            mediaType: 'photo',
+            includeBase64: false,
+            maxHeight: 2000,
+            maxWidth: 2000
+        };
+
+        launchImageLibrary(options, (response)=>{
+            if(response.didCancel){
+                console.log('cancelado pelo usuário')
+            } else if(response.error){
+                console.log('erro ao abrir a galeria')
+            } else {
+                let imageUri = response.uri || response.assets?.[0]?.uri;
+                setImagem(imageUri)
+            }
+        })
+
+
+
+    }
+
 
     return (
         <View style={styles.container}>
@@ -43,37 +88,43 @@ const CadastroProduto: React.FC = () => {
             <Text style={styles.headerText}>Top food</Text>
         </View>
           <View style={styles.form} >
-            <TextInput style={styles.input} placeholder="Nome do Produto" value={nome} />
+            <TextInput 
+            style={styles.input} 
+            placeholder="Nome do Produto" 
+            value={nome}
+            onChangeText={setNome} />
+
             <TextInput 
             style={styles.input}
             placeholder="Preço"
             value={preco}
             onChangeText={setPreco} />
+
             <TextInput 
             style={styles.input}
             placeholder="Ingredientes"
             value={ingredientes}
             onChangeText={setIngredientes}
             multiline />
-          </View>
+         
 
           <View style={styles.alinhamentoImagensSelecionada} > 
           {imagem ? <Image source={{ uri: imagem}} style={styles.imageSelecionada} /> : null}  
          </View>
 
-         <TouchableOpacity style={styles.imageButton}>
+         <TouchableOpacity style={styles.imageButton} onPress={selecionarImagem}>
             <Text style={styles.imageButtonText} > Selecionar Imagens</Text>
          </TouchableOpacity>
 
-         <TouchableOpacity style={styles.imageButton}>
-            <Text style={styles.imageButtonText} onPress={abrirCamera} > Tirar Foto</Text>
+         <TouchableOpacity style={styles.imageButton} onPress={abrirCamera} >
+            <Text style={styles.imageButtonText}  > Tirar Foto</Text>
          </TouchableOpacity>
 
-         <TouchableOpacity style={styles.imageButton}>
+         <TouchableOpacity style={styles.imageButton} onPress={cadastrarProduto}>
             <Text style={styles.imageButtonText} > Cadastrar Produto</Text>
          </TouchableOpacity>
          </View>
-        
+         </View>
 
     );
 }
