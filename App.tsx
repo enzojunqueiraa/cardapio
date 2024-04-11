e9949fc090e3a7a1ee6faf17ff630f9f35a9546c
enzojunqueiraa <enzobjunqueira@gmail.com>
2024-04-11 15:13:20 -0300

    commit de hj

--- App.tsx
+++ App.tsx
@@ -7,11 +7,11 @@ function App(): React.JSX.Element {
 
 
   return (
- // < Cardapio />
+   < Cardapio />
 
- // < CadastroProduto />
+ //< CadastroProduto />
 
-  <CadastroCliente />
+ // <CadastroCliente />
 
   );
 }
